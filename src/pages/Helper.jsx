9f3258772ef3afda483f67de07106dd1a32a20f3
Super-Sigma-67 <s_franciscoasuncion.tena@ousd.org>
2026-04-22 10:13:49 -0700

    this sucks!

--- src/pages/Helper.jsx
+++ src/pages/Helper.jsx
@@ -1,9 +1,10 @@
+import Card from "../components/Card";
+
 function Helper() {
   return ( 
   <> 
     <h1>Helper Page</h1> 
-    <h1> How to use the subscription helper:</h1>
-    <p>To use the subscription helper, simply input your subscriptions into the table and it will show you how much you are spending each month on subscriptions! It will also show you what subscriptions you have and a helpful visual to show you how much you are spending on subscriptions each month!</p> 
+    <Card myTitle="Coyos Subscription helper Us" mySubtitle="Directions:" myBlurb="Type in your subscription, how much money it is a month and then save! Repeat as many times as needed :)" ></Card>
 
    
   </>
