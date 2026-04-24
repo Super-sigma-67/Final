85f268778163e4c0548a44ba66aeedcf7cd80b30
Super-Sigma-67 <s_franciscoasuncion.tena@ousd.org>
2026-04-24 10:16:19 -0700

    afdfsa!

--- src/pages/Helper.jsx
+++ src/pages/Helper.jsx
@@ -5,7 +5,7 @@ function Helper() {
   <> 
   <div className = "bg-fuchsia-600">
     <h1>Helper Page</h1> 
-    <Card myTitle="Coyos Subscription helper Us" mySubtitle="Directions:" myBlurb="Type in your subscription, how much money it is a month and then save! Repeat as many times as needed :)" ></Card>
+    <Card myTitle="Coyos Subscription helper" mySubtitle="Directions:" myBlurb="Type in your subscription, how much money it is a month and then save! Repeat as many times as needed :)" ></Card>
 </div>
    
   </>
